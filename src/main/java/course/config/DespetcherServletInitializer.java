b3d2100738a7339bea5fe265a927cae005fbd0de
JAVACspb <al.alentjev@yandex.ru>
2022-10-26 22:43:10 +0300

    Initial commit

---
+++ src/main/java/course/config/DespetcherServletInitializer.java
@@ -0,0 +1,20 @@
+package course.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DespetcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
---
+++ src/main/java/course/config/controllers/FirstController.java
@@ -0,0 +1,18 @@
+package course.config.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/first")
+public class FirstController {
+    @GetMapping("/hello")
+    public String helloPage(){
+        return "first/hello";
+    }
+    @GetMapping("/by")
+    public String byPage(){
+        return "first/by";
+    }
+}
