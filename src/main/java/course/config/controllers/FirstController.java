856aaf37876cfc35f4d9fe4b7d1a10d81e013918
JAVACspb <al.alentjev@yandex.ru>
2022-10-28 14:14:02 +0300

    Annotation:RequestParam HttpServletRequest request

--- src/main/java/course/config/controllers/FirstController.java
+++ src/main/java/course/config/controllers/FirstController.java
@@ -3,14 +3,31 @@ package course.config.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
-    @GetMapping("/hello")
-    public String helloPage(){
-        return "first/hello";
-    }
+
+
+//    @GetMapping("/hello")
+//    public String helloPage(HttpServletRequest request){
+//        String name = request.getParameter("name");
+//        String surname = request.getParameter("surname");
+//        System.out.println("Hello !" + name + "" + surname);
+//        return "first/hello";
+//    }
+
+
+
+@GetMapping("/hello")
+public String helloPage(@RequestParam("name") String name,
+                        @RequestParam("surname") String surname){
+    System.out.println("Hello !" + name + "" + surname);
+    return "first/hello";
+}
     @GetMapping("/by")
     public String byPage(){
         return "first/by";
